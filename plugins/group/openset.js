ef9f314366cca561fdeb0e12416b2bc1f8e38c34
wanvz <ridwanramadhan2006@gmail.com>
2026-01-21 08:15:06 +0700

    Update openset.js

--- plugins/group/openset.js
+++ plugins/group/openset.js
@@ -1,49 +1,27 @@
-const { schedules } = require('../../src/lib/scheduler/groupSchedule')
+// plugins/group/openset.js
+const { schedules } = require('../../src/lib/scheduler/groupSchedule');
 
 const pluginConfig = {
-
     name: 'openset',
-
     alias: ['setopen'],
-
     category: 'group',
-
     description: 'Set jadwal auto open grup',
-
     usage: '.openset 08:00',
-
     isGroup: true,
-
     isAdmin: true,
-
     isBotAdmin: true
+};
 
+async function handler(m, { args }) {
+    if (!args[0]) return m.reply('Contoh: .openset 08:00');
+    if (!/^\d{2}:\d{2}$/.test(args[0])) return m.reply('Format salah (HH:mm)');
+
+    const data = schedules.get(m.chat) || {};
+    data.open = args[0];
+    data.last = null;
+    schedules.set(m.chat, data);
+
+    m.reply(` Auto OPEN diset jam *${args[0]}*`);
 }
 
-async function handler(m, { args = [] }) {
-
-    if (!args[0]) return m.reply('Contoh: .openset 08:00')
-
-    if (!/^\d{2}:\d{2}$/.test(args[0]))
-
-        return m.reply('Format salah (HH:mm)')
-
-    const data = schedules.get(m.chat) || {}
-
-    data.open = args[0]
-
-    data.last = null
-
-    schedules.set(m.chat, data)
-
-    m.reply(`🔓 Auto OPEN diset jam *${args[0]}*`)
-
-}
-
-module.exports = {
-
-    config: pluginConfig,
-
-    handler
-
-}
+module.exports = { config: pluginConfig, handler };
